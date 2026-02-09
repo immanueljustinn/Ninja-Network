a3e6f2868d294177c2a9ef656d49942b5ba959e7
Immanuel Justin <immanueljustin@example.com>
2026-02-09 22:26:07 +0700

    first commit

---
+++ resources/views/ninjas/create.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi nisi totam quos quaerat aliquam sit sint distinctio et commodi beatae perspiciatis fuga, a natus. Delectus error corporis cum architecto aspernatur!</p>
+</x-layout>
